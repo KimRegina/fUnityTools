61bb4a00fbd99e6a005840872ae889929f14ef18
xiangcaoyuyan <xiangcaoyuyan@gmail.com>
2023-09-03 23:23:34 +0800

    添加快捷键功能到FUnityTools

--- Editor/FEditorTools.cs
+++ Editor/FEditorTools.cs
@@ -1,11 +1,23 @@
 using UnityEditor;
-using UnityEngine;
 
-public class FEditorTools
+//********************************************************
+// % (ctrl on Windows and Linux, cmd on macOS),
+// ^ (ctrl on Windows, Linux, and macOS),
+// # (shift),
+// & (alt).
+////********************************************************
+
+public class FEditorTools : Editor
 {
-    [MenuItem("FUnityTools/TestMenu")]
-    public static void TestMenu()
+    [MenuItem("FUnityTools ShortCuts/开关gameObjects显示 %&A")]
+    public static void SetSelectionsActive()
     {
-        Debug.Log("this is fEditorTools");
+        var list = Selection.gameObjects;
+        if (list == null || list.Length == 0) return;
+        bool isActive = list[0].activeInHierarchy;
+        for (int i = 0; i < list.Length; i++)
+        {
+            list[i].SetActive(!isActive);
+        }
     }
 }
